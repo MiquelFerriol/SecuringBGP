f32a586273d029426bad51729b1ef5d5d9eaff52
Miquel Ferriol <miquel.ferriol@est.fib.upc.edu>
2019-11-21 21:18:53 +0100

    chanelBlock

---
+++ contract/lib/communitylist.js
@@ -0,0 +1,33 @@
+/*
+SPDX-License-Identifier: Apache-2.0
+*/
+
+'use strict';
+
+// Utility class for collections of ledger states --  a state list
+const StateList = require('./../ledger-api/statelist.js');
+
+const Community = require('./community.js');
+
+class CommunityList extends StateList {
+
+    constructor(ctx) {
+        super(ctx,'securingBGP.community');
+        this.use(Community);
+    }
+
+    async addCommunity(community) {
+        return this.addState(community);
+    }
+
+    async getCommunity(communityKey) {
+        return this.getState(communityKey);
+    }
+
+    async updateCommunity(community) {
+        return this.updateState(community);
+    }
+}
+
+
+module.exports = CommunityList;
