df733b64ae183aade35a49c81ab340ae8905bd34
Miquel Ferriol <miquel.ferriol@est.fib.upc.edu>
2019-11-22 17:30:48 +0100

    getPublicIP and Docker-Compose ENV

--- Utils/getPublicIP.js
+++ Utils/getPublicIP.js
@@ -3,16 +3,24 @@ var AWS = require('aws-sdk');
 AWS.config.loadFromPath('./config.json');
 var ec2 = new AWS.EC2();
 
-var params = {
-    InstanceIds: [
-        "i-1234567890abcdef0"
-    ]
-};
-ec2.describeInstances(params, function(err, data) {
+
+ec2.describeInstances({}, function(err, data) {
     if (err) console.log(err, err.stack); // an error occurred
-    else     console.log(data);           // successful response
-    /*
-    data = {
+    else{
+        var buf = '';
+        data['Reservations'].forEach(function (value) {
+            let instance = value['Instances'][0];
+            buf += instance['Tags'][0].Value + "_IP="+instance['PublicIpAddress']+'\n';
+
+        });
+
+        const fs = require('fs');
+
+        buf = buf.toUpperCase();
+
+        fs.writeFile("../aws-network/deployment/.env", buf, (err) => {
+            if (err) console.log(err);
+            console.log(buf);
+        });
     }
-    */
 });
